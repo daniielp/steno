1cef9ecf8838b830070d2fb0c953652ad3d685b2
simon767b <simon767b@gmail.com>
2024-04-11 14:07:48 +0200

    forside og slutside

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,14 @@
 /*------------------
 -------index--------
 ------------------*/
+const armbåndsknap = document.getElementsByClassName("blinker"); 
+
+armbåndsknap[0].addEventListener("click", function() {
+    window.location.href = "https://www.w3schools.com/jsref/met_element_addeventlistener.asp";
+    console.log("armbåndsknap");
+  });
+
+  armbåndsknap[1].addEventListener("click", function() {
+    window.location.href = "https://www.w3schools.com/jsref/met_element_addeventlistener.asp";
+    console.log("armbåndsknap");
+  });
